ecaddf98a39d50f82cd0bfa21be547d9d8cc459c
gkira-hub <54792309+gkira-hub@users.noreply.github.com>
2020-05-05 00:39:34 +0530

    Add files via upload

---
+++ i)_a)_insertion sort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n,temp,x,y;
+    printf("enter size of array:");
+    scanf("%d",&n);
+    int a[n];
+    printf("enter elements of array:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("unsorted array:");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    for( x=1;x<n;x++)
+    {
+        temp=a[x];
+        for(y=x-1;y>=0 && a[y]>temp;y--)
+        {
+            a[y+1]=a[y];
+        }
+        a[y+1]=temp;
+    }
+    printf("\nsorted array:");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+}
